08c4229eea59423465e0997da7ba1641218314ef
rmfish <rmfish.io@gmail.com>
2022-11-08 18:00:11 +0800

    index_dailybasic 接口ok

---
+++ tutake/api/tushare/extends/index_dailybasic_ext.py
@@ -0,0 +1,94 @@
+import pendulum
+
+from tutake.api.tushare.process import ProcessType
+
+
+def default_order_by_ext(self) -> str:
+    """
+    查询时默认的排序
+    """
+    return 'trade_date desc,ts_code'
+
+
+def default_limit_ext(self) -> str:
+    """
+    每次取数的默认Limit
+    """
+    return '3000'
+
+
+def prepare_ext(self, process_type: ProcessType):
+    """
+    同步历史数据准备工作
+    """
+
+
+def tushare_parameters_ext(self, process_type: ProcessType):
+    """
+    同步历史数据调用的参数
+    :return: list(dict)
+    000016.SH,上证50
+    000905.SH,中证500
+    399001.SZ,深证成指
+    399005.SZ,中小板指
+    399006.SZ,创业板指
+    399905.SZ,中证500
+    """
+
+    return [{'ts_code': '000016.SH'}, {'ts_code': '000905.SH'}, {'ts_code': '399001.SZ'}, {'ts_code': '399005.SZ'},
+            {'ts_code': '399006.SZ'}, {'ts_code': '399905.SZ'}]
+# params = []
+#     str_format = "YYYYMMDD"
+#     start_record_date = pendulum.parse('20140101')  # 最早的数据记录
+#     day_period = 500
+#     if ProcessType.HISTORY == process_type:
+#         min_date = self.min("trade_date", condition="trade_date is not null")
+#         if min_date is not None:
+#             end_date = pendulum.parse(min_date).add(days=-1)
+#         else:
+#             end_date = pendulum.now()
+#         while end_date > start_record_date:
+#             start_date = end_date.add(days=-day_period)
+#             params.append({"start_date": start_date.format(str_format), "end_date": end_date.format(str_format)})
+#             end_date = start_date.add(days=-day_period)
+#     else:
+#         max_date = self.max("trade_date")
+#         if max_date is not None:
+#             start_date = pendulum.parse(max_date)
+#         else:
+#             start_date = start_record_date.add(days=-1)
+#         while start_date.diff(pendulum.now(), False).in_hours() > 0:
+#             end_date = start_date.add(days=day_period)
+#             params.append({"start_date": start_date.format(str_format), "end_date": end_date.format(str_format)})
+#             start_date = start_date.add(days=day_period)
+#     return params
+
+def param_loop_process_ext(self, process_type: ProcessType, **params):
+    """
+    每执行一次fetch_and_append前，做一次参数的处理，如果返回None就中断这次执行
+    """
+    date_format = 'YYYYMMDD'
+    if process_type == ProcessType.HISTORY:
+        min_date = self.min("trade_date", "ts_code = '%s'" % params['ts_code'])
+        if min_date is None:
+            params['end_date'] = ""
+        elif params.get('list_date') == min_date:
+            # 如果时间相等不用执行
+            return None
+        else:
+            min_date = pendulum.parse(min_date)
+            end_date = min_date.add(days=-1)
+            params['end_date'] = end_date.format(date_format)
+        return params
+    else:
+        max_date = self.max("trade_date", "ts_code = '%s'" % params['ts_code'])
+        if max_date is None:
+            params['start_date'] = ""
+        elif max_date == pendulum.now().format(date_format):
+            # 如果已经是最新时间
+            return None
+        else:
+            max_date = pendulum.parse(max_date)
+            start_date = max_date.add(days=1)
+            params['start_date'] = start_date.format(date_format)
+        return params
---
+++ tutake/api/tushare/index_dailybasic.py
@@ -0,0 +1,162 @@
+"""
+This file is auto generator by CodeGenerator. Don't modify it directly, instead alter tushare_api.tmpl of it.
+
+Tushare index_dailybasic接口
+数据接口-指数-大盘指数每日指标  https://tushare.pro/document/2?doc_id=128
+
+@author: rmfish
+"""
+import pandas as pd
+import logging
+from sqlalchemy import Integer, String, Float, Column, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from tutake.api.tushare.base_dao import BaseDao
+from tutake.api.tushare.dao import DAO
+from tutake.api.tushare.extends.index_dailybasic_ext import *
+from tutake.api.tushare.process import ProcessType, DataProcess
+from tutake.api.tushare.tushare_base import TuShareBase
+from tutake.utils.config import tutake_config
+from tutake.utils.decorator import sleep
+
+engine = create_engine("%s/%s" % (tutake_config.get_data_sqlite_driver_url(), 'tushare_index_dailybasic.db'))
+session_factory = sessionmaker()
+session_factory.configure(bind=engine)
+Base = declarative_base()
+logger = logging.getLogger('api.tushare.index_dailybasic')
+
+
+class TushareIndexDailybasic(Base):
+    __tablename__ = "tushare_index_dailybasic"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ts_code = Column(String, index=True, comment='TS代码')
+    trade_date = Column(String, index=True, comment='交易日期')
+    total_mv = Column(Float, comment='当日总市值')
+    float_mv = Column(Float, comment='当日流通市值')
+    total_share = Column(Float, comment='当日总股本')
+    float_share = Column(Float, comment='当日流通股本')
+    free_share = Column(Float, comment='当日自由流通股本')
+    turnover_rate = Column(Float, comment='换手率')
+    turnover_rate_f = Column(Float, comment='换手率(自由流通股本)')
+    pe = Column(Float, comment='市盈率')
+    pe_ttm = Column(Float, comment='市盈率TTM')
+    pb = Column(Float, comment='市净率')
+
+
+TushareIndexDailybasic.__table__.create(bind=engine, checkfirst=True)
+
+
+class IndexDailybasic(BaseDao, TuShareBase, DataProcess):
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        query_fields = [
+            n for n in [
+                'trade_date',
+                'ts_code',
+                'start_date',
+                'end_date',
+                'limit',
+                'offset',
+            ] if n not in ['limit', 'offset']
+        ]
+        entity_fields = [
+            "ts_code", "trade_date", "total_mv", "float_mv", "total_share", "float_share", "free_share",
+            "turnover_rate", "turnover_rate_f", "pe", "pe_ttm", "pb"
+        ]
+        BaseDao.__init__(self, engine, session_factory, TushareIndexDailybasic, 'tushare_index_dailybasic',
+                         query_fields, entity_fields)
+        TuShareBase.__init__(self)
+        DataProcess.__init__(self, "index_dailybasic")
+        self.dao = DAO()
+
+    def index_dailybasic(self, fields='', **kwargs):
+        """
+        大盘指数每日指标
+        | Arguments:
+        | trade_date(str):   交易日期
+        | ts_code(str):   TS指数代码
+        | start_date(str):   开始日期
+        | end_date(str):   结束日期
+        | limit(int):   单次返回数据长度
+        | offset(int):   请求数据的开始位移量
+        
+        :return: DataFrame
+         ts_code(str)  TS代码
+         trade_date(str)  交易日期
+         total_mv(float)  当日总市值
+         float_mv(float)  当日流通市值
+         total_share(float)  当日总股本
+         float_share(float)  当日流通股本
+         free_share(float)  当日自由流通股本
+         turnover_rate(float)  换手率
+         turnover_rate_f(float)  换手率(自由流通股本)
+         pe(float)  市盈率
+         pe_ttm(float)  市盈率TTM
+         pb(float)  市净率
+        
+        """
+        return super().query(fields, **kwargs)
+
+    def process(self, process_type: ProcessType):
+        """
+        同步历史数据
+        :return:
+        """
+        return super()._process(process_type, self.fetch_and_append)
+
+    def fetch_and_append(self, **kwargs):
+        """
+        获取tushare数据并append到数据库中
+        :return: 数量行数
+        """
+        init_args = {"trade_date": "", "ts_code": "", "start_date": "", "end_date": "", "limit": "", "offset": ""}
+        if len(kwargs.keys()) == 0:
+            kwargs = init_args
+        # 初始化offset和limit
+        if not kwargs.get("limit"):
+            kwargs['limit'] = self.default_limit()
+        init_offset = 0
+        offset = 0
+        if kwargs.get('offset'):
+            offset = int(kwargs['offset'])
+            init_offset = offset
+
+        kwargs = {key: kwargs[key] for key in kwargs.keys() & init_args.keys()}
+
+        @sleep(timeout=61, time_append=60, retry=20, match="^抱歉，您每分钟最多访问该接口")
+        def fetch_save(offset_val=0):
+            kwargs['offset'] = str(offset_val)
+            logger.debug("Invoke pro.index_dailybasic with args: {}".format(kwargs))
+            res = self.tushare_api().index_dailybasic(**kwargs, fields=self.entity_fields)
+            res.to_sql('tushare_index_dailybasic', con=engine, if_exists='append', index=False, index_label=['ts_code'])
+            return res
+
+        df = fetch_save(offset)
+        offset += df.shape[0]
+        while kwargs['limit'] != "" and str(df.shape[0]) == kwargs['limit']:
+            df = fetch_save(offset)
+            offset += df.shape[0]
+        return offset - init_offset
+
+
+setattr(IndexDailybasic, 'default_limit', default_limit_ext)
+setattr(IndexDailybasic, 'default_order_by', default_order_by_ext)
+setattr(IndexDailybasic, 'prepare', prepare_ext)
+setattr(IndexDailybasic, 'tushare_parameters', tushare_parameters_ext)
+setattr(IndexDailybasic, 'param_loop_process', param_loop_process_ext)
+
+if __name__ == '__main__':
+    pd.set_option('display.max_columns', 500)    # 显示列数
+    pd.set_option('display.width', 1000)
+    logger.setLevel(logging.INFO)
+    api = IndexDailybasic()
+    # api.process(ProcessType.HISTORY)    # 同步历史数据
+    api.process(ProcessType.INCREASE)  # 同步增量数据
+    print(api.index_dailybasic())    # 数据查询接口
