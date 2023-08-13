cf3c6b5e82f4ebf5725650ee4e961f6e3688b0f7
rmfish <rmfish.io@gmail.com>
2023-08-13 14:13:51 +0800

    增加index_weekly接口

---
+++ tutake/api/ts/index_weekly.py
@@ -0,0 +1,209 @@
+"""
+This file is auto generator by CodeGenerator. Don't modify it directly, instead alter tushare_api.tmpl of it.
+
+Tushare index_weekly接口
+指数周线行情
+数据接口-指数-指数周线行情  https://tushare.pro/document/2?doc_id=171
+
+@author: rmfish
+"""
+import pandas as pd
+import tushare as ts
+from sqlalchemy import Integer, String, Float, Column, create_engine
+from sqlalchemy.orm import sessionmaker
+
+from tutake.api.base_dao import Base
+from tutake.api.process import DataProcess
+from tutake.api.process_report import ProcessException
+from tutake.api.ts.index_weekly_ext import *
+from tutake.api.ts.tushare_dao import TushareDAO
+from tutake.api.ts.tushare_api import TushareAPI
+from tutake.api.ts.tushare_base import TuShareBase
+from tutake.utils.config import TutakeConfig
+from tutake.utils.utils import project_root
+
+
+class TushareIndexWeekly(Base):
+    __tablename__ = "tushare_index_weekly"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ts_code = Column(String, index=True, comment='TS指数代码')
+    trade_date = Column(String, index=True, comment='交易日')
+    close = Column(Float, comment='收盘点位')
+    open = Column(Float, comment='开盘点位')
+    high = Column(Float, comment='最高点位')
+    low = Column(Float, comment='最低点位')
+    pre_close = Column(Float, comment='昨日收盘点')
+    change = Column(Float, comment='涨跌点位')
+    pct_chg = Column(Float, comment='涨跌幅')
+    vol = Column(Float, comment='成交量')
+    amount = Column(Float, comment='成交额')
+
+
+class IndexWeekly(TushareDAO, TuShareBase, DataProcess):
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __init__(self, config):
+        self.engine = create_engine(config.get_data_sqlite_driver_url('tushare_index_weekly.db'),
+                                    connect_args={
+                                        'check_same_thread': False,
+                                        'timeout': config.get_sqlite_timeout()
+                                    })
+        session_factory = sessionmaker()
+        session_factory.configure(bind=self.engine)
+        TushareIndexWeekly.__table__.create(bind=self.engine, checkfirst=True)
+
+        query_fields = ['ts_code', 'trade_date', 'start_date', 'end_date', 'limit', 'offset']
+        entity_fields = [
+            "ts_code", "trade_date", "close", "open", "high", "low", "pre_close", "change", "pct_chg", "vol", "amount"
+        ]
+        TushareDAO.__init__(self, self.engine, session_factory, TushareIndexWeekly, 'tushare_index_weekly.db',
+                            'tushare_index_weekly', query_fields, entity_fields, config)
+        DataProcess.__init__(self, "index_weekly", config)
+        TuShareBase.__init__(self, "index_weekly", config, 600)
+        self.api = TushareAPI(config)
+
+    def columns_meta(self):
+        return [{
+            "name": "ts_code",
+            "type": "String",
+            "comment": "TS指数代码"
+        }, {
+            "name": "trade_date",
+            "type": "String",
+            "comment": "交易日"
+        }, {
+            "name": "close",
+            "type": "Float",
+            "comment": "收盘点位"
+        }, {
+            "name": "open",
+            "type": "Float",
+            "comment": "开盘点位"
+        }, {
+            "name": "high",
+            "type": "Float",
+            "comment": "最高点位"
+        }, {
+            "name": "low",
+            "type": "Float",
+            "comment": "最低点位"
+        }, {
+            "name": "pre_close",
+            "type": "Float",
+            "comment": "昨日收盘点"
+        }, {
+            "name": "change",
+            "type": "Float",
+            "comment": "涨跌点位"
+        }, {
+            "name": "pct_chg",
+            "type": "Float",
+            "comment": "涨跌幅"
+        }, {
+            "name": "vol",
+            "type": "Float",
+            "comment": "成交量"
+        }, {
+            "name": "amount",
+            "type": "Float",
+            "comment": "成交额"
+        }]
+
+    def index_weekly(self, fields='', **kwargs):
+        """
+        指数周线行情
+        | Arguments:
+        | ts_code(str):   TS代码
+        | trade_date(str):   交易日期
+        | start_date(str):   开始日期
+        | end_date(str):   结束日期
+        | limit(int):   单次返回数据长度
+        | offset(int):   请求数据的开始位移量
+        
+        :return: DataFrame
+         ts_code(str)  TS指数代码
+         trade_date(str)  交易日
+         close(float)  收盘点位
+         open(float)  开盘点位
+         high(float)  最高点位
+         low(float)  最低点位
+         pre_close(float)  昨日收盘点
+         change(float)  涨跌点位
+         pct_chg(float)  涨跌幅
+         vol(float)  成交量
+         amount(float)  成交额
+        
+        """
+        return super().query(fields, **kwargs)
+
+    def process(self):
+        """
+        同步历史数据
+        :return:
+        """
+        return super()._process(self.fetch_and_append)
+
+    def fetch_and_append(self, **kwargs):
+        """
+        获取tushare数据并append到数据库中
+        :return: 数量行数
+        """
+        init_args = {"ts_code": "", "trade_date": "", "start_date": "", "end_date": "", "limit": "", "offset": ""}
+        if len(kwargs.keys()) == 0:
+            kwargs = init_args
+        # 初始化offset和limit
+        if not kwargs.get("limit"):
+            kwargs['limit'] = self.default_limit()
+        init_offset = 0
+        offset = 0
+        if kwargs.get('offset'):
+            offset = int(kwargs['offset'])
+            init_offset = offset
+
+        kwargs = {key: kwargs[key] for key in kwargs.keys() & init_args.keys()}
+
+        def fetch_save(offset_val=0):
+            try:
+                kwargs['offset'] = str(offset_val)
+                self.logger.debug("Invoke pro.index_weekly with args: {}".format(kwargs))
+                res = self.tushare_query('index_weekly', fields=self.entity_fields, **kwargs)
+                res.to_sql('tushare_index_weekly',
+                           con=self.engine,
+                           if_exists='append',
+                           index=False,
+                           index_label=['ts_code'])
+                return res
+            except Exception as err:
+                raise ProcessException(kwargs, err)
+
+        df = fetch_save(offset)
+        offset += df.shape[0]
+        while kwargs['limit'] != "" and str(df.shape[0]) == kwargs['limit']:
+            df = fetch_save(offset)
+            offset += df.shape[0]
+        return offset - init_offset
+
+
+setattr(IndexWeekly, 'default_limit', default_limit_ext)
+setattr(IndexWeekly, 'default_cron_express', default_cron_express_ext)
+setattr(IndexWeekly, 'default_order_by', default_order_by_ext)
+setattr(IndexWeekly, 'prepare', prepare_ext)
+setattr(IndexWeekly, 'query_parameters', query_parameters_ext)
+setattr(IndexWeekly, 'param_loop_process', param_loop_process_ext)
+
+if __name__ == '__main__':
+    pd.set_option('display.max_columns', 50)    # 显示列数
+    pd.set_option('display.width', 100)
+    config = TutakeConfig(project_root())
+    pro = ts.pro_api(config.get_tushare_token())
+    # print(pro.index_weekly(trade_date='19910704'))
+    # print(pro.index_weekly(trade_date='19910705',start_date='19910101',end_date='20000101'))
+
+    api = IndexWeekly(config)
+    # api.process()    # 同步增量数据
+    print(api.index_weekly())    # 数据查询接口
---
+++ tutake/api/ts/index_weekly_ext.py
@@ -0,0 +1,45 @@
+"""
+Tushare index_weekly接口
+指数周线行情
+数据接口-指数-指数周线行情  https://tushare.pro/document/2?doc_id=171
+"""
+from tutake.api.ts.date_utils import start_end_step_params, day_by_day_params
+
+
+def default_cron_express_ext(self) -> str:
+    return ""
+
+
+def default_order_by_ext(self) -> str:
+    """
+    查询时默认的排序
+    """
+    return 'ts_code,trade_date'
+
+
+def default_limit_ext(self) -> str:
+    """
+    每次取数的默认Limit
+    """
+    return "1000"
+
+
+def prepare_ext(self):
+    """
+    同步历史数据准备工作
+    """
+
+
+def query_parameters_ext(self):
+    """
+    同步历史数据调用的参数
+    :return: list(dict)
+    """
+    return day_by_day_params(self, '19901218', date_column='trade_date')
+
+
+def param_loop_process_ext(self, **params):
+    """
+    每执行一次fetch_and_append前，做一次参数的处理，如果返回None就中断这次执行
+    """
+    return params
