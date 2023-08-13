7f2f6d9334705fe1bfa3151429c4fd5e99a0de82
rmfish <rmfish.io@gmail.com>
2023-08-13 14:49:03 +0800

    增加index_weight接口

---
+++ tutake/api/ts/sz_daily_info_ext.py
@@ -0,0 +1,45 @@
+"""
+Tushare sz_daily_info接口
+获取深圳市场每日交易概况
+数据接口-指数-深圳市场每日交易情况  https://tushare.pro/document/2?doc_id=268
+"""
+from tutake.api.ts.date_utils import start_end_step_params
+
+
+def default_cron_express_ext(self) -> str:
+    return ""
+
+
+def default_order_by_ext(self) -> str:
+    """
+    查询时默认的排序
+    """
+    return 'trade_date,ts_code'
+
+
+def default_limit_ext(self) -> str:
+    """
+    每次取数的默认Limit
+    """
+    return "2000"
+
+
+def prepare_ext(self):
+    """
+    同步历史数据准备工作
+    """
+
+
+def query_parameters_ext(self):
+    """
+    同步历史数据调用的参数
+    :return: list(dict)
+    """
+    return start_end_step_params(self, start_date='20080102', step=30)
+
+
+def param_loop_process_ext(self, **params):
+    """
+    每执行一次fetch_and_append前，做一次参数的处理，如果返回None就中断这次执行
+    """
+    return params
